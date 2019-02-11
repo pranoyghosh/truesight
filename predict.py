eecaa9e7665c5e2531e90845e5558f9362b42d74
harshichebolu@gmail.com <pranoyghosh.ghosh@gmail.com>
2019-02-11 12:10:53 +0530

    minor changes to predict.py

--- predict.py
+++ predict.py
@@ -30,12 +30,12 @@ images = images / 255.0
 #testAttrX = df
 testImagesX = images
 # load json and create model
-json_file = open('model1.json', 'r')
+json_file = open('models/model1.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("model1.h5")
+loaded_model.load_weights("models/model1.h5")
 print("Loaded model from disk")
 
 # make predictions on the testing data
