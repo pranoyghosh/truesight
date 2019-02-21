6e91e3a4527614fae7db33d3ae1e9296e8d66686
harshichebolu@gmail.com <pranoyghosh.ghosh@gmail.com>
2019-02-21 12:58:06 +0530

    changes major

--- mixed_training.py
+++ mixed_training.py
@@ -12,39 +12,42 @@ from keras.models import model_from_json
 
 
 print("[INFO] loading attributes...")
-inputPath = "/home/harshit1201/Desktop/Project:TrueSight/Dataset/training.csv"
-df = datasets.load_attributes(inputPath)
+inputPath = "/home/harshit1201/Desktop/Project:TrueSight/training_set.csv"
+cols =["image_name"]
+files=[]
+df = pd.read_csv(inputPath, skiprows=[0], header=None, names=cols)
+for filename,p,n,e in df.index.values:
+    files.append(os.path.join(inputPath,filename))
+colsdf = ["image_name","x1", "x2", "y1", "y2"]
+df = pd.read_csv(inputPath, skiprows=[0], header=None, names=colsdf)
+img_data = "/home/harshit1201/Desktop/Project:TrueSight/Dataset/images"
+#images = datasets.load_images(inputPath, img_data)
+#images = images / 255.0
 
-# load the object images and then scale the pixel intensities to the
-# range [0, 1]
-print("[INFO] loading images...")
-img_data = "/home/harshit1201/Desktop/Project:TrueSight/Dataset/training"
-images = datasets.load_images(inputPath, img_data)
-images = images / 255.0
-
-print(images.shape)
-print(df.shape)
+#print(images.shape)
+#print(df.shape)
 # partition the data into training and testing splits using 95% of
 # the data for training and the remaining 5% for validation
 print("[INFO] processing data...")
-split = train_test_split(df, images, test_size=0.10, random_state=33)
-(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
-print(trainAttrX.shape)
+#split = train_test_split(df, images, test_size=0.10, random_state=33)
+#(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
+#print(trainAttrX.shape)
 # find the largest  bounding box coordinate for each x1 x2 y1 y2 in the training set and use it to
 # scale our bounding box coordinates to the range [0, 1] (will lead to better
 # training and convergence)
-maxX1 = trainAttrX["x1"].max()
-trainY1 = trainAttrX["x1"] / maxX1
-testY1 = testAttrX["x1"] / maxX1
-maxX2 = trainAttrX["x2"].max()
-trainY2 = trainAttrX["x2"] / maxX2
-testY2 = testAttrX["x2"] / maxX2
-maxY1 = trainAttrX["y1"].max()
-trainY3 = trainAttrX["y1"] / maxY1
-testY3 = testAttrX["y1"] / maxY1
-maxY2 = trainAttrX["y2"].max()
-trainY4 = trainAttrX["y2"] / maxY2
-testY4 = testAttrX["y2"] / maxY2
+'''
+maxX1 = df["x1"].max()
+trainY1 = df["x1"] / maxX1
+#testY1 = testAttrX["x1"] / maxX1
+maxX2 = df["x2"].max()
+trainY2 = df["x2"] / maxX2
+#testY2 = testAttrX["x2"] / maxX2
+maxY1 = df["y1"].max()
+trainY3 = df["y1"] / maxY1
+#testY3 = testAttrX["y1"] / maxY1
+maxY2 = df["y2"].max()
+trainY4 = df["y2"] / maxY2'''
+#testY4 = testAttrX["y2"] / maxY2
 
 cnn = models.create_cnn(128, 128, 3, regress=False)
 
@@ -56,8 +59,9 @@ x1 = Dense(1, activation="linear")(x)
 x2 = Dense(1, activation="linear")(x)
 y1 = Dense(1, activation="linear")(x)
 y2 = Dense(1, activation="linear")(x)
+combinedOutput = concatenate([x1,x2,y1,y2])
 
-model = Model(inputs=cnn.input, outputs=[x1, x2, y1, y2])
+model = Model(inputs=cnn.input, outputs=combinedOutput)
 
 
 opt = Adam(lr=1e-3, decay=1e-3 / 60)
@@ -67,18 +71,17 @@ model.compile(optimizer=opt, loss='mean_squared_error', metrics=[iou.mean_iou])
 # train the model
 print("[INFO] training model...")
 model.fit(
-    trainImagesX, [trainY1,trainY2,trainY3,trainY4],
-    validation_data=(testImagesX, [testY1,testY2,testY3,testY4]),
-    epochs=60, batch_size=6)
+    np.array(datasets.load_images(inputPath, img_data),ndmin=4), np.array(datasets.load_attributes(inputPath)),
+    epochs=60, batch_size=None, steps_per_epoch=3)
 
 # evaluate the model
-scores = model.evaluate(trainImagesX, [trainY1,trainY2,trainY3,trainY4], verbose=0)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+#scores = model.evaluate(trainImagesX, [trainY1,trainY2,trainY3,trainY4], verbose=0)
+#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # serialize model to JSON
 model_json = model.to_json()
-with open("models/model2.json", "w") as json_file:
+with open("models/model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save_weights("models/model2.h5")
+model.save_weights("models/model.h5")
 print("Saved model to disk")
--- module/datasets.py
+++ module/datasets.py
@@ -7,12 +7,12 @@ import glob
 import cv2
 import os
 
-def load_attributes(inputPath):
-    cols = ["x1", "x2", "y1", "y2"]
-    df = pd.read_csv(inputPath, skiprows=[0], header=None, names=cols)
+def load_attributes(df,fname):
+    out=df.loc[df["image_name"]==fname]
+    del out["image_name"]
+    out=out.values.tolist()
     # return the data frame
-    return df
-
+    return out
 
 def load_images(inpdf, inputPath):
     # initialize our images array
@@ -27,3 +27,65 @@ def load_images(inpdf, inputPath):
         images.append(c)
         # return our set of images
     return np.array(images)
+
+def custom_genimg(files,df,bsize=8000):
+    batch_paths=np.random.choice(a=files,size=bsize)
+    batch_input=[]
+    batch_output=[]
+    for input_path in batch_paths:
+        input=load_images(input_path)
+        fname=os.path.basename(input_path)
+        output=load_attributes(df,fname)
+        input=preprocess_img(input)
+        output=preprocess_att(output)
+        batch_input+=[input]
+        batch_output+=[output]
+    batch_x=np.array(batch_input)
+    batch_y=np.array(batch_output)
+    yield(batch_x, batch_y)
+
+def preprocess_att(inputPath, bsize=8000):
+    cols = ["x1", "x2", "y1", "y2"]
+    df = pd.read_csv(inputPath, skiprows=[0], header=None, names=cols)
+    L=len(df)
+    maxX1 = df["x1"].max()
+    df["x1"] = df["x1"] / maxX1
+    maxX2 = df["x2"].max()
+    df["x2"] = df["x2"] / maxX2
+    maxY1 = df["y1"].max()
+    df["y1"] = df["y1"] / maxY1
+    maxY2 = df["y2"].max()
+    df["y2"] = df["y2"] / maxY2
+    while True:
+        bstart=0
+        bend=bsize
+        while bstart < L:
+            df=df.values[bstart:bend]
+            # return the data frame
+            return df
+        bstart+=bsize
+        bend+=bsize
+
+
+
+def preprocess_img(inpdf, inputPath, bsize=8000):
+    cols =["image_name"]
+    df = pd.read_csv(inpdf, skiprows=[0], header=None, names=cols)
+    L=len(df)
+    while True:
+        # initialize our images array
+
+        bstart=0
+        bend=bsize
+        while bstart < L:
+            images = []
+            # loop over the indexes of the houses
+            for filename,p,n,e in df.index.values[bstart:bend]:
+                img = cv2.imread(os.path.join(inputPath,filename))
+                c=cv2.resize(img, (128,128))
+                c=c/255
+                images.append(c)
+                # return our set of images
+            return images
+        bstart+=bsize
+        bend+=bsize
